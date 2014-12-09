51430b4b77d32a95e4d3b4e3f11183f682ddc36c
Dorina <dorinuta895@gmail.com>
2014-12-09 12:22:57 +0200

    first commit

---
+++ application/views/news/success.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Create</title>
+</head>
+<body>
+
+<h3>Form was successfully submitted!</h3>
+
+<p><?php echo anchor('news/create', 'Try it again!'); ?></p>
+
+</body>
+</html>
---
+++ system/core/Model.php
@@ -0,0 +1,84 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeIgniter
+ *
+ * An open source application development framework for PHP 5.1.6 or newer
+ *
+ * @package		CodeIgniter
+ * @author		ExpressionEngine Dev Team
+ * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc.
+ * @license		http://codeigniter.com/user_guide/license.html
+ * @link		http://codeigniter.com
+ * @since		Version 1.0
+ * @filesource
+ */
+
+// ------------------------------------------------------------------------
+
+/**
+ * CodeIgniter Model Class
+ *
+ * @package		CodeIgniter
+ * @subpackage	Libraries
+ * @category	Libraries
+ * @author		ExpressionEngine Dev Team
+ * @link		http://codeigniter.com/user_guide/libraries/config.html
+ */
+class CI_Model {
+
+	/**
+	 * Constructor
+	 *
+	 * @access public
+	 */
+	function __construct()
+	{
+		log_message('debug', "Model Class Initialized");
+	}
+
+	/**
+	 * __get
+	 *
+	 * Allows models to access CI's loaded classes using the same
+	 * syntax as controllers.
+	 *
+	 * @param	string
+	 * @access private
+	 */
+	function __get($key)
+	{
+		$CI =& get_instance();
+		return $CI->$key;
+	}
+        
+        public function get_news($slug = FALSE)
+        {
+                if ($slug === FALSE)
+                {
+                        $query = $this->db->get('news');
+                        return $query->result_array();
+                }
+
+                $query = $this->db->get_where('news', array('slug' => $slug));
+                return $query->row_array();
+        }
+        
+        public function set_news()
+        {
+                $this->load->helper('url');
+
+                $slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+                $data = array(
+                        'title' => $this->input->post('title'),
+                        'slug' => $slug,
+                        'text' => $this->input->post('text')
+                );
+
+                return $this->db->insert('news', $data);
+        }
+}
+// END Model Class
+
+/* End of file Model.php */
+/* Location: ./system/core/Model.php */

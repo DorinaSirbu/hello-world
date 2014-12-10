93776e8eaf292ed245ac560a38696ff50ed1359a
Dorina <dorinuta895@gmail.com>
2014-12-10 11:42:46 +0200

    testing model

--- system/core/Model.php
+++ system/core/Model.php
@@ -76,7 +76,32 @@ class CI_Model {
                 );
 
                 return $this->db->insert('news', $data);
+//blog_model
         }
+        function get_last_ten_entries()
+        {
+                $query = $this->db->get('entries', 10);
+                return $query->result();
+        }
+
+        function insert_entry()
+        {
+                $this->title   = $_POST['title']; // please read the below note
+                $this->content = $_POST['content'];
+                $this->date    = time();
+
+                $this->db->insert('entries', $this);
+        }
+
+        function update_entry()
+        {
+                $this->title   = $_POST['title'];
+                $this->content = $_POST['content'];
+                $this->date    = time();
+
+                $this->db->update('entries', $this, array('id' => $_POST['id']));
+        }
+
 }
 // END Model Class
 
